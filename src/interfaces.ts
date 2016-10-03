cc321173e69c3b05988259044f476ff8cf402a86
Rick Carlino <rick.carlino@gmail.com>
2016-10-02 21:47:16 -0500

    Typo in stepType interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -67,21 +67,21 @@
 
   /** Names for a single step within a sequence. */
   export type stepType = "emergency_stop"
-    | "home_all"
-    | "home_x"
-    | "home_y"
-    | "home_z"
-    | "move_absolute"
-    | "move_relative"
-    | "pin_write"
-    | "read_parameter"
-    | "read_status"
-    | "write_parameter"
-    | "wait"
-    | "send_message"
-    | "if_statement"
-    | "read_pin"
-    | "execute";
+                          | "home_all"
+                          | "home_x"
+                          | "home_y"
+                          | "home_z"
+                          | "move_absolute"
+                          | "move_relative"
+                          | "pin_write"
+                          | "read_parameter"
+                          | "read_status"
+                          | "write_parameter"
+                          | "wait"
+                          | "send_message"
+                          | "if_statement"
+                          | "read_pin"
+                          | "execute";
 
   /** Color choices for sequence tiles. */
   export type Color = "blue"
