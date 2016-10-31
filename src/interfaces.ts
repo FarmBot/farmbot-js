67bdd1ec14f92d931c357497fb379b404c7b4a34
Rick Carlino <rick.carlino@gmail.com>
2016-10-31 13:45:14 -0500

    v2.1.5

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -67,6 +67,7 @@ export type Pins = { [num: string]: Pin | undefined };
 export interface Configuration {
   os_auto_update?: boolean | undefined;
   fw_auto_update?: boolean | undefined;
+  arbitrary_config?: string | undefined;
 }
 
 export interface InformationalSettings {
