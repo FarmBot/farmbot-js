0614a90180535abfb23cbd0aa97ff6aab69728ae
Rick Carlino <rick.carlino@gmail.com>
2017-04-11 12:40:10 -0500

    Version 3.3.1: Add encoder types

--- dist/farmbot.js
+++ dist/farmbot.js
@@ -380,6 +380,6 @@ var Farmbot = (function () {
     };
     return Farmbot;
 }());
-Farmbot.VERSION = "3.3.0";
+Farmbot.VERSION = "3.3.1";
 Farmbot.defaults = { speed: 800, timeout: 6000 };
 exports.Farmbot = Farmbot;
