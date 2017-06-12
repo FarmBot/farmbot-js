7f0b09a3b44b880db855ff890e7ddbe4887a1abd
Rick Carlino <rick.carlino@gmail.com>
2017-06-12 09:44:30 -0500

    V4.0.5

--- dist/farmbot.js
+++ dist/farmbot.js
@@ -412,6 +412,6 @@ var Farmbot = (function () {
     };
     return Farmbot;
 }());
-Farmbot.VERSION = "4.0.4";
+Farmbot.VERSION = "4.0.5";
 Farmbot.defaults = { speed: 800, timeout: 6000, secure: true };
 exports.Farmbot = Farmbot;
