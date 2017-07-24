23ee0090ef50ed42190fdd91c31ac7201b97fc90
Rick Carlino <rick.carlino@gmail.com>
2017-07-24 08:52:21 -0500

    Update build.sh to auto-run docs

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ rm -rf dist
 mkdir dist
 node_modules/typescript/bin/tsc
 
+# Update Documentation
+typedoc --out ./doc/ src/farmbot.ts
+
 # Commit
 echo "Now run this: "
 echo "    git commit -am 'FBJS Version x.y.z'"
