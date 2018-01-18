ea6a986ab80ed521fbc3a69958935a188f0d993c
Rick Carlino <rick.carlino@gmail.com>
2018-01-18 10:26:33 -0700

    v5.3.3 - better error reporting

--- src/farmbot.ts
+++ src/farmbot.ts
@@ -29,7 +29,7 @@ declare var global: typeof window;
 const RECONNECT_THROTTLE = 1000;
 
 export class Farmbot {
-  static VERSION = "5.3.2";
+  static VERSION = "5.3.3";
   static defaults = { speed: 100, timeout: 15000 };
 
   /** Storage area for all event handlers */
@@ -361,6 +361,7 @@ export class Farmbot {
             handler(data, event);
           } catch (e) {
             console.warn("Exception thrown while handling `" + event + "` event.");
+            console.dir(e);
           }
         });
       });
