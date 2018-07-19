598505eef6bfbeb2ce0f2b9be4f11c79c9374cf3
Rick Carlino <rick.carlino@gmail.com>
2018-07-19 11:36:06 -0500

    v6.1.3

--- dist/farmbot.js
+++ dist/farmbot.js
@@ -376,7 +376,7 @@ var Farmbot = /** @class */ (function () {
             }
         });
     };
-    Farmbot.VERSION = "6.1.2";
+    Farmbot.VERSION = "6.1.3";
     return Farmbot;
 }());
 exports.Farmbot = Farmbot;
