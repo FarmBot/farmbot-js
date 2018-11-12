3a8aa1f258e008a28d9edd31694e355c284ef1d2
Rick Carlino <rick.carlino@gmail.com>
2018-11-12 07:50:13 -0600

    Add new field to FbosConfig

--- src/farmbot.ts
+++ src/farmbot.ts
@@ -39,7 +39,7 @@ export class Farmbot {
   private config: Conf;
   public client?: MqttClient;
   public resources: ResourceAdapter;
-  static VERSION = "6.5.8";
+  static VERSION = "6.5.9";
 
   constructor(input: FarmbotConstructorParams) {
     this._events = {};
