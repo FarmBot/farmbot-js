db10ed5e10147dd7ca4917e570ac1a6b85537537
Rick Carlino <rick.carlino@gmail.com>
2018-11-12 07:52:53 -0600

    v6.5.9

--- dist/resources/configs/fbos.d.ts
+++ dist/resources/configs/fbos.d.ts
@@ -16,7 +16,9 @@ export interface FbosConfig {
     api_migrated: boolean;
     os_auto_update: boolean;
     arduino_debug_messages: boolean;
+    firmware_path: string;
+    firmware_debug_log: boolean;
 }
 export declare type NumberConfigKey = "id" | "device_id" | "network_not_found_timer";
-export declare type StringConfigKey = "firmware_hardware";
-export declare type BooleanConfigKey = "auto_sync" | "beta_opt_in" | "disable_factory_reset" | "firmware_input_log" | "firmware_output_log" | "sequence_body_log" | "sequence_complete_log" | "sequence_init_log" | "api_migrated" | "os_auto_update" | "arduino_debug_messages";
+export declare type StringConfigKey = "firmware_hardware" | "firmware_path";
+export declare type BooleanConfigKey = "auto_sync" | "api_migrated" | "arduino_debug_messages" | "beta_opt_in" | "disable_factory_reset" | "firmware_debug_log" | "firmware_input_log" | "firmware_output_log" | "os_auto_update" | "sequence_body_log" | "sequence_complete_log" | "sequence_init_log";
