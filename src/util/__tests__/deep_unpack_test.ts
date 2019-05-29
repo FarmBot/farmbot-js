fd2a76d1647a8a38db4deb8c8913e172502bfa50
Rick Carlino <rick.carlino@gmail.com>
2019-05-29 17:33:16 -0500

    add test for deepUnpack() with an object

--- src/util/__tests__/deep_unpack_test.ts
+++ src/util/__tests__/deep_unpack_test.ts
@@ -1,8 +1,20 @@
 import { deepUnpack } from "../deep_unpack";
+import { Vector3 } from "../../interfaces";
 
 describe("deepUnpack()", () => {
   const fooBar =
     (baz: number | boolean | string | undefined) => ({ foo: { bar: { baz } } });
+
+  it("unpacks Vector3", () => {
+    /** NOTE: This is a test to ensure that
+     *  objects such as a vec3 can still be
+     *  unpacked. FBOS occasionally needs to send
+     *  fully-formed objects. */
+    const vec3: Vector3 = { x: -1, y: 2, z: 9 };
+    const x: any = deepUnpack("foo.bar.baz", vec3 as any);
+    expect(x.foo.bar.baz).toEqual(vec3);
+  });
+
   it("unpacks 'dot properties'", () => {
     expect(deepUnpack("foo.bar.baz", 1)).toEqual(fooBar(1));
     expect(deepUnpack("foo.bar.baz", true)).toEqual(fooBar(true));
