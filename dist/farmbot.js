5236fd8d9a1cdd2034456f32599a5471639d21dc
Rick Carlino <rick.carlino@gmail.com>
2019-08-23 12:39:21 -0500

    8.1.5

--- dist/farmbot.js
+++ dist/farmbot.js
@@ -498,7 +498,7 @@ var Farmbot = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
-    Farmbot.VERSION = "8.1.2";
+    Farmbot.VERSION = "8.1.5";
     return Farmbot;
 }());
 exports.Farmbot = Farmbot;
--- dist/resources/configs/web_app.d.ts
+++ dist/resources/configs/web_app.d.ts
@@ -53,6 +53,6 @@ export interface WebAppConfig {
     z_axis_inverted: boolean;
     zoom_level: number;
 }
-export declare type NumberConfigKey = "id" | "bot_origin_quadrant" | "busy_log" | "debug_log" | "device_id" | "error_log" | "fun_log" | "info_log" | "map_size_x" | "map_size_y" | "success_log" | "warn_log" | "zoom_level";
+export declare type NumberConfigKey = "id" | "device_id" | "assertion_log" | "busy_log" | "debug_log" | "error_log" | "fun_log" | "info_log" | "success_log" | "warn_log" | "bot_origin_quadrant" | "map_size_x" | "map_size_y" | "zoom_level";
 export declare type StringConfigKey = "created_at" | "photo_filter_begin" | "photo_filter_end" | "updated_at";
 export declare type BooleanConfigKey = "confirm_plant_deletion" | "confirm_sequence_deletion" | "confirm_step_deletion" | "disable_animations" | "disable_emergency_unlock_confirmation" | "disable_i18n" | "discard_unsaved_sequences" | "discard_unsaved" | "display_trail" | "dynamic_map" | "enable_browser_speak" | "encoder_figure" | "expand_step_options" | "hide_sensors" | "hide_webcam_widget" | "home_button_homing" | "legend_menu_open" | "raw_encoders" | "scaled_encoders" | "show_farmbot" | "show_first_party_farmware" | "show_historic_points" | "show_images" | "show_motor_plot" | "show_pins" | "show_plants" | "show_points" | "show_sensor_readings" | "show_spread" | "stub_config" | "time_format_24_hour" | "user_interface_read_only_mode" | "x_axis_inverted" | "xy_swap" | "y_axis_inverted" | "z_axis_inverted";
