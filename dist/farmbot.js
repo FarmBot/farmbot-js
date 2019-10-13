b1aedc801da2d99b331382665eec1902c772c5af
Rick Carlino <rick.carlino@gmail.com>
2019-10-13 14:01:18 -0500

    v8.3.0-rc6

--- dist/farmbot.js
+++ dist/farmbot.js
@@ -505,7 +505,7 @@ var Farmbot = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
-    Farmbot.VERSION = "8.3.0-rc5";
+    Farmbot.VERSION = "8.3.0-rc6";
     return Farmbot;
 }());
 exports.Farmbot = Farmbot;
--- src/farmbot.ts
+++ src/farmbot.ts
@@ -46,7 +46,7 @@ export class Farmbot {
   private config: Conf;
   public client?: MqttClient;
   public resources: ResourceAdapter;
-  static VERSION = "8.3.0-rc5";
+  static VERSION = "8.3.0-rc6";
 
   constructor(input: FarmbotConstructorParams) {
     this._events = {};
