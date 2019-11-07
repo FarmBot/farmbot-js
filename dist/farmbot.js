87e474cc73fec50912aef4a24afa26e2e8baa3cf
Rick Carlino <rick.carlino@gmail.com>
2019-11-07 14:38:52 -0600

    v8.3.0

--- dist/farmbot.js
+++ dist/farmbot.js
@@ -505,7 +505,7 @@ var Farmbot = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
-    Farmbot.VERSION = "8.3.0-rc6";
+    Farmbot.VERSION = "8.3.0";
     return Farmbot;
 }());
 exports.Farmbot = Farmbot;
