f8b08e5e1122eb884189bbe509dcb44862914384
Rick Carlino <rick.carlino@gmail.com>
2020-06-12 16:10:05 -0500

    v10.1.1

--- src/farmbot.ts
+++ src/farmbot.ts
@@ -46,7 +46,7 @@ export class Farmbot {
   private config: Conf;
   public client?: MqttClient;
   public resources: ResourceAdapter;
-  static VERSION = "10.1.0";
+  static VERSION = "10.1.1";
 
   constructor(input: FarmbotConstructorParams) {
     this._events = {};
