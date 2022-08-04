fb47595314c0a5514b8a970796a1dad817f75024
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2022-08-04 10:17:50 -0700

    fix build

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ rm -rf dist
 mkdir dist
 node_modules/typescript/bin/tsc
 node_modules/.bin/parcel build
+status=$?
 # Commit
 echo "Now run this: "
 echo "    git add -A"
 echo "    git commit -am 'FBJS Version x.y.z'"
 echo "    git push ____ main"
 echo "    npm publish"
+exit $status
